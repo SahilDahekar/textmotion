63cfa0658e68ac18c6631b0e074c9e642b6b3787
SahilDahekar <dahekar30sahil@gmail.com>
2025-06-14 14:01:52 +0530

    Fixed clerk key issue

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-# Replace environment variables in the built JS files
-find /usr/share/nginx/html -type f -name "*.js" -exec sed -i "s|VITE_CLERK_PUBLISHABLE_KEY_PLACEHOLDER|$VITE_CLERK_PUBLISHABLE_KEY|g" {} +
+# Replace environment variables in the JS files
+find /usr/share/nginx/html -type f -name "*.js" -exec sed -i "s|VITE_CLERK_PUBLISHABLE_KEY_PLACEHOLDER|${VITE_CLERK_PUBLISHABLE_KEY}|g" {} +
 
-# Start nginx
-nginx -g "daemon off;"
+# Generate env-config.js with runtime values
+echo "window.VITE_CLERK_PUBLISHABLE_KEY='${VITE_CLERK_PUBLISHABLE_KEY}';" > /usr/share/nginx/html/env-config.js
+
+# Execute the original entrypoint
+exec "$@"
